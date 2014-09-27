1065e9bb45860f4922c57e1f3db459b652720710
David-Lee Kulsch <coding@kulsch-it.de>
2014-09-27 21:48:30 +0200

    adding music, sfx

--- Breakout/source/PlayState.hx
+++ Breakout/source/PlayState.hx
@@ -7,13 +7,18 @@ import flixel.FlxState;
 import flixel.group.FlxGroup;
 import flixel.util.FlxColor;
 import flixel.util.FlxRandom;
+import flixel.ui.FlxButton;
 import flixel.ui.FlxVirtualPad;
-
+import flixel.text.FlxText;
+import flixel.util.FlxStringUtil;
 /**
 * Atari 2600 Breakout
 * 
 * @author Davey, Photon Storm
 * @link http://www.photonstorm.com/archives/1290/video-of-me-coding-breakout-in-flixel-in-20-mins
+*
+* rewrite for android inclluding virtualPad and more by David-Lee Kulsch
+* github: http://github.com/the1andonlydave/flx-demo-fork in breakout
 */
 class PlayState extends FlxState
 {
@@ -27,10 +32,16 @@ class PlayState extends FlxState
 	private var _rightWall:FlxSprite;
 	private var _topWall:FlxSprite;
 	private var _bottomWall:FlxSprite;
+	private var _floor:FlxGroup;
+	
 	
 	private var _bricks:FlxGroup;
 	
 	public static var virtualPad:FlxVirtualPad;
+	
+	private var score:Int;
+	private var ball:Int;
+	private var _hudball:FlxText;  
 	
 	override public function create():Void
 	{
@@ -66,7 +77,9 @@ class PlayState extends FlxState
 		_bottomWall = new FlxSprite(0, 239);
 		_bottomWall.makeGraphic(320, 10, FlxColor.TRANSPARENT);
 		_bottomWall.immovable = true;
-		_walls.add(_bottomWall);
+		//_walls.add(_bottomWall);
+		_floor = new FlxGroup();
+		_floor.add(_bottomWall);
 		
 		// Some bricks
 		_bricks = new FlxGroup();
@@ -95,9 +108,24 @@ class PlayState extends FlxState
 		add(_bat);
 		add(_ball);
 		add(_bricks);
+		add(_floor);
+		
+		// HUD - score and ball count
+		_hudball = new FlxText(0, 0, FlxG.width);
+		_hudball.setFormat(null, 16, FlxColor.YELLOW, "center", FlxText.BORDER_OUTLINE, 0x131c1b);
+		_hudball.scrollFactor.set(0, 0);
+		add(_hudball);		
+	
+
 		virtualPad = new FlxVirtualPad(LEFT_RIGHT, A);
 		virtualPad.setAll("alpha", 0.5);
 		add(virtualPad);	 //add last  = foreground
+		
+		score=0; //TODO add something like oldscore=score for making red if lower and green (in _hudball) if score>oldscore. but how do we transfer in gameover() to playstate, but on first run we need to initialize it with a value in onCreate
+		ball=3;//just in case
+		
+		FlxG.sound.playMusic("assets/music/background_1.ogg",true); //true enable looping
+	
 	}
 	
 	override public function update():Void
@@ -106,6 +134,8 @@ class PlayState extends FlxState
 		
 		_bat.velocity.x = 0;
 		
+		updateHud();
+
 		#if !FLX_NO_TOUCH
 		// Simple routine to move bat to x position of touch
 		for (touch in FlxG.touches.list)
@@ -129,21 +159,20 @@ class PlayState extends FlxState
 		}
 		#end
 		
-		//
-		//
-		//
-		//	_left = FlxG.keys.anyPressed(["LEFT", "A"])  || MenuState.virtualPad.buttonLeft.status == FlxButton.PRESSED;
-		//_right = FlxG.keys.anyPressed(["RIGHT", "D"]) || MenuState.virtualPad.buttonA.status == FlxButton.PRESSED;	
-		if (FlxG.keys.anyPressed(["LEFT", "A"]) && _bat.x > 10)
+		if ((FlxG.keys.anyPressed(["LEFT", "A"])  || PlayState.virtualPad.buttonLeft.status == FlxButton.PRESSED )&& _bat.x > 10)
 		{
 			_bat.velocity.x = - BAT_SPEED;
 		}
-		else if (FlxG.keys.anyPressed(["RIGHT", "D"]) && _bat.x < 270)
+		else if ((FlxG.keys.anyPressed(["RIGHT", "D"]) || PlayState.virtualPad.buttonA.status == FlxButton.PRESSED || PlayState.virtualPad.buttonRight.status == FlxButton.PRESSED)&& _bat.x < 270)
 		{
 			_bat.velocity.x = BAT_SPEED;
 		}
+		else
+		{	//TODO:do we need a if flxbutton.release for left, right and A-Button here too?
+			_bat.velocity.x =0;//stop moving if key released on virtualpad
+		}
 		
-		if (FlxG.keys.justReleased.R)
+		if (FlxG.keys.justReleased.R) //TODO map it to onscreen button or something on android, or move into pausemenu on press of overlaybutton or back-key(@override in .java file with template?)
 		{
 			FlxG.resetState();
 		}
@@ -161,11 +190,16 @@ class PlayState extends FlxState
 		FlxG.collide(_ball, _walls);
 		FlxG.collide(_bat, _ball, ping);
 		FlxG.collide(_ball, _bricks, hit);
+		FlxG.collide(_ball, _floor, floor);	
 	}
 	
 	private function hit(Ball:FlxObject, Brick:FlxObject):Void
 	{
+		trace("score increase");
 		Brick.exists = false;
+		score++;
+		FlxG.sound.play("assets/sfx/_brickdestroy", 1, false);	//123
+		Ball.velocity.y += 10; //beginn with 200 and increase on every block collision
 	}
 	
 	private function ping(Bat:FlxObject, Ball:FlxObject):Void
@@ -191,6 +225,41 @@ class PlayState extends FlxState
 			// Ball is perfectly in the middle
 			// A little random X to stop it bouncing up!
 			Ball.velocity.x = 2 + FlxRandom.intRanged(0, 8);
+		}
+	}
+	
+	private function floor(Ball:FlxObject, Floor:FlxObject):Void
+	{
+		trace("floor");
+		var batmid:Int = Std.int(Bat.x) + 20;
+		trace ("batmid="+batmid);
+		//TODO make screen shake/flicker 
+		//FlxG.camera.flash(0xff000000, 1);
+		ball--;//decrease ballcounter
+		_ball.x=batmid;
+		_ball.y=210;//10 above batmid
+		_ball.velocity.y = 200;//slow down
+		//TODO ball.exists = false;
+	}
+	
+	private function gameover():Void
+	{
+		trace("gameover");
+		//TODO make screen shake/flicker FlxG.camera.flash(0xff000000, 1);
+		ball=3;//for next turn
+		FlxG.switchState(new PlayState()); //TODO check if working or do we need something like state(current_state) like in unity 3d?
+	}
+	
+	private function updateHud():Void
+	{
+		if(ball==1){		trace("1 ball");
+			_hudball.text=Std.string(ball)+" Ball remaining "+Std.string(score)+" Score";
+			}
+		else if(ball>1){	trace("more than 1 ball");
+			_hudball.text=Std.string(ball)+" Balls remaining "+Std.string(score)+" Score";
+			}
+		else{	trace("empty ball");
+			gameover();
 		}
 	}
 }
